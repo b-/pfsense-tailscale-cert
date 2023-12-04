8dcabab1ce8d6e0258bea3f09e1a1fcce5ee09d6
Josh Stompro <github@stompro.org>
2023-12-04 13:58:50 -0600

    Support Captive Portal and Unbound
    Setup the new cert for use in the Captive Portal and Unbound.
    
    Importing the CA doesn't work yet, so if you are using Letsencrypt with the
    Captive Portal, import that with the web UI once.
    
    Signed-off-by: Josh Stompro <github@stompro.org>

--- pfsense-import-certificate.php
+++ pfsense-import-certificate.php
@@ -11,7 +11,7 @@ if (empty($argc)) {
 }
 
 if ($argc != 3) {
-	echo "Usage: php " . $argv[0] . " /path/to/certificate.crt /path/to/private/key.pem\r\n";
+	echo "Usage: php " . $argv[0] . " /path/to/certificate.crt /path/to/private/key.pem /path/to/chain.pem \r\n";
 	die(1);
 }
 
@@ -23,6 +23,9 @@ require_once "shaper.inc";
 
 $certificate = trim(file_get_contents($argv[1]));
 $key = trim(file_get_contents($argv[2]));
+if ($argv[3]) { #If CA specified load it.
+  $ca = trim(file_get_contents($argv[3]));
+}
 
 // Do some quick verification of the certificate, similar to what the GUI does
 if (empty($certificate)) {
@@ -90,13 +93,49 @@ sleep(3); //sleep to space out the write_config calls so they show distinctly
 // Assuming that all worked, we now need to set the new certificate for use in the GUI
 $config['system']['webgui']['ssl-certref'] = $cert['refid'];
 
+// If Unbound is set to use TLS, then set the new certificate to be used by Unbound also.
+if (isset($config['unbound']['enable']) and isset($config['unbound']['enablessl'])) {
+  echo "Install Unbound Certificate\r\n";
+  $config['unbound']['sslcertref'] = $cert['refid'];
+  $restartunbound = true;
+}
+
+// If captive portal is set to use TLS, then set the new certificate
+//   to be used by captive portals
+foreach ($config['captiveportal'] as $cpid => $cportal) {
+  if(isset($cportal['enable']) and isset($cportal['httpslogin'])){
+    echo "Update Captive Portal Certificate: $cpid\r\n";
+    $config['captiveportal'][$cpid]['certref'] = $cert['refid'];
+  }
+}
+
+
 write_config("Set new certificate as active for webgui, from pfsense-import-certificate.php");
 sleep(3); //sleep to space out the write_config calls
 
-log_error(gettext("webConfigurator configuration has changed. Restarting webConfigurator."));
-send_event("service restart webgui");
+//Use cert_get_all_services to grab all services now using the new cert.
+
+$services = cert_get_all_services($cert['refid']);
+
+//Restart all services that are using the new cert.
+echo "Restart services that are using the new cert\r\n";
+print_r($services);
+log_error(gettext("Restart services that are using the new cert"));
+
+cert_restart_services($services);
+//All except unbound, there is a bug in cert_restart_services... it doesn't restart unbound
+//https://redmine.pfsense.org/issues/15062
 
 echo "Completed! New certificate installed.\r\n";
+
+// If unbound cert was updated, then restart unbound.
+// Remove once bug fixed
+if ($restartunbound) {
+  echo "Restart Unbound\n";
+  log_error(gettext("Unbound configuration has changed. Restarting Unbound."));
+  service_control_restart('unbound','');
+}
+
 
 // Delete unused certificates added by this script
 
